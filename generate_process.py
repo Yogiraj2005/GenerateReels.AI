e77930bc5378ade99d68161690cfa4757a11a09b
Yogiraj2005 <yogirajshinde357@gmail.com>
2026-01-01 12:24:01 +0530

    The elevenlabs is set now And Happy new year

--- generate_process.py
+++ generate_process.py
@@ -1,19 +1,27 @@
 import os
+from text_to_audio import text_to_speech_file
+import time
 
 def text_to_audio(folder):
     print("TTA", folder)
+    with open(f"user_uploads/{folder}/desc.txt") as f:
+        text = f.read()
+    print(text, folder)
 
 def create_reel(folder):
     print("CR", folder)
 
 if __name__ == "__main__":
-    with open("done.txt", "r") as f:
-        done_folders = f.readlines()
+    while True:
+        print("prodessing.......")
+        with open("done.txt", "r") as f:
+            done_folders = f.readlines()
 
-    folders = os.listdir("user_uploads")
-    for folder in folders:
-        if(folder not in done_folders):
-            text_to_audio(folder)
-            create_reel(folder)
-            with open("done.txt", "a") as f:
-                f.write(folder + "\n")
+        folders = os.listdir("user_uploads")
+        for folder in folders:
+            if(folder not in done_folders):
+                text_to_audio(folder)
+                create_reel(folder)
+                with open("done.txt", "a") as f:
+                    f.write(folder + "\n")
+        time.sleep(4)
