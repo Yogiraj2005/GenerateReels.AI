9c6ab0569d111062073170b74062df7beab9bf37
Yogiraj2005 <yogirajshinde357@gmail.com>
2026-01-02 12:48:03 +0530

    The API key is blocking my ip address need to fix it

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ def create():
         print(request.files.keys())
         rec_id = request.form.get("uuid")
         desc = request.form.get("text")
+        input_files = []
         for key, value in request.files.items():
             print(key, value)
    
@@ -32,13 +33,19 @@ def create():
                 if(not(os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'],rec_id)))):
                     os.mkdir(os.path.join(app.config['UPLOAD_FOLDER'],rec_id))
                 file.save(os.path.join(app.config['UPLOAD_FOLDER'],rec_id, filename))
+                input_files.append(file.filename)
             # Captury the text and save to the file
             with open (os.path.join(app.config['UPLOAD_FOLDER'],rec_id, "desc.txt"), "w") as f:
                 f.write(desc)
+        for fl in input_files:
+            with open(os.path.join(app.config['UPLOAD_FOLDER'], rec_id, "input.txt"), "a") as f:
+                f.write(f"file '{fl}'\n duration \n")
     return render_template("create.html", myid = myid)
 
 @app.route("/gallery")
 def gallery():
-    return render_template("gallery.html")
+    reels = os.listdir("static/reels")
+    print(reels)
+    return render_template("gallery.html", reels= reels)
 
 app.run(debug=True)
